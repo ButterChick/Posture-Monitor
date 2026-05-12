018e667e3e59a34fcf43b5e43daa1f280c144fd8
ButterChick <dhruvphooli@gmail.com>
2026-05-12 11:19:05 +0530

    Almost done with the web app, it is detecting the person, just have to change the index.html file a lil and add a beeping sound like in the local app

--- App.py
+++ App.py
@@ -11,6 +11,9 @@ import time
 from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
 
 MODEL_PATH = "pose_landmarker_full.task"
 MODEL_URL = "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task"
@@ -24,7 +27,7 @@ posture_model = tf.keras.models.load_model("posture_lm.h5")
 
 LANDMARK_INDICES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-GOOD_CLASS_INDEX = 0
+GOOD_CLASS_INDEX = 1
 
 options = PoseLandmarkerOptions(
     base_options = python.BaseOptions(model_asset_path = MODEL_PATH),
@@ -34,6 +37,12 @@ options = PoseLandmarkerOptions(
 landmarker = PoseLandmarker.create_from_options(options)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -43,18 +52,19 @@ async def predict(file: UploadFile = File(...)):
 
     mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data = frame)
     result = landmarker.detect(mp_image)
+    print(f"Landmarks found: {len(result.pose_landmarks)}")
 
-    if not result.pose_landmarker:
-        return JSONResponse({"Status":"No person Found"})
+    if not result.pose_landmarks:
+        return JSONResponse({"status":"No person Found"})
     
-    lm = result.pose_landmarker[0]
+    lm = result.pose_landmarks[0]
 
-    if lm<13:
-        return JSONResponse({"status": "partial", "confidence": 0})
+    if len(lm) < 13:
+        return JSONResponse({"status": "partial"})
     row = []
-    for i in landmarker:
+    for i in LANDMARK_INDICES:
         p = lm[i]
-        row = [p.x,p.y,p.z]
+        row += [p.x,p.y,p.z]
 
     input_vec = np.array(row,dtype=np.float32).reshape(1,39)
     prediction = posture_model.predict(input_vec,verbose=0)[0][0]
@@ -63,6 +73,6 @@ async def predict(file: UploadFile = File(...)):
     good_posture = good_confidence > 0.65
 
     return JSONResponse({
-        "Status" : "Good" if good_posture else "bad"
+        "status" : "good" if good_posture else "bad"
     })
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
